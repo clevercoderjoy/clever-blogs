b34e765827c843560c265dcbd2004696e2ac3e8d
clevercoderjoy <clevercoderjoy@gmail.com>
2024-11-01 18:20:46 +0530

    feat: header in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
+
 const Header = () => {
   return (
     <>
-      header
+      <header>
+        <div className="logo">
+          <span>CLEVER</span>
+          <span>BLOGS</span>
+        </div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Blogs</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        <div>
+          <button>Dark Mode</button>
+          <button>Sign Up</button>
+        </div>
+      </header>
     </>
   )
 }
 
-export default Header
+export default Header;
